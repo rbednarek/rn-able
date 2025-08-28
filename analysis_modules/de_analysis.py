9ecc38719c63d786619adde3056d7b3cac9e8847
Ryland <rybednarek@gmail.com>
2025-08-28 13:28:36 -0700

    pca functionality

---
+++ analysis_modules/de_analysis.py
@@ -0,0 +1,49 @@
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def plot_count_pca(count_df, 
+                   raw_count=10, 
+                   min_samples=3, 
+                   components=2, 
+                   log=True, 
+                   plot=True,
+                   meta_df=None,
+                   col1=None,
+                   col2=None,
+                   exp=None):
+    pca = PCA(n_components=components)
+    count_filter = count_df.loc[:, (count_df > raw_count).sum(axis=0) >= min_samples]
+    if log:
+        count_filter = np.log1p(count_filter)
+    pca_result = pca.fit_transform(count_filter.T)
+    variance = pca.explained_variance_ratio_ *100
+    pc1_variance = round(variance[0], 2)
+    pc2_variance = round(variance[1], 2)
+
+    pca_df = pd.DataFrame(pca_result, columns=['PC1', 'PC2'], index=count_filter.columns)
+    if plot:
+        plt.figure(figsize=(8, 6))
+        if meta_df is not None:
+            merged_df = pca_df.join(meta_df, how='left')
+            markers = ['o', 's', '^', 'D', 'X', 'P', '*']
+            if col2 and col2 in merged_df.columns:
+                for (color, shape), group in merged_df.groupby([col1, col2]):
+                    plt.scatter(group['PC1'], group['PC2'], label=f'{color}-{shape}', marker=markers[hash(shape) % len(markers)])
+            elif col1 and col1 in merged_df.columns:
+                for color, group in merged_df.groupby(col1):
+                    plt.scatter(group['PC1'], group['PC2'], label=color)
+        else:
+            plt.scatter(pca_df['PC1'], pca_df['PC2'])
+        if exp:
+            plt.title(f'{exp} - Count Data PCA')
+        else:
+            plt.title('Count Data PCA')    
+        plt.xlabel(f'PC1 ({pc1_variance}%)')
+        plt.ylabel(f'PC2 ({pc2_variance}%)')
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+        plt.grid()
+        plt.show()
+    return pca_df
+
--- analysis_modules/file_handling.py
+++ analysis_modules/file_handling.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import gzip
 
 def read_csv(file_path, index_col=0):
     '''Reads a CSV or TSV file into a DataFrame'''
@@ -13,3 +14,26 @@ def read_csv(file_path, index_col=0):
 def write_csv(df, file_path, index=False):
     df.to_csv(file_path, index=index)
 
+
+def geo_series_matrix_to_df(geo_txt,
+                            accessions='!Sample_geo_accession',
+                            condition1='!Sample_source_name_ch1',
+                            condition2=None):
+    with gzip.open(geo_txt, 'rt') as f:
+        for i in f:
+            if i.startswith(accessions):
+                sample_names = i.strip().split('\t')[1:]
+            elif i.startswith(condition1):
+                sample_condition = i.strip().split('\t')[1:]
+            elif condition2 and i.startswith(condition2):
+                sample_condition2 = i.strip().split('\t')[1:]
+    if sample_names and sample_condition:
+        df = pd.DataFrame({'sample': sample_names, 'condition': sample_condition})
+        df.applymap(lambda x: x.replace('"', '') if isinstance(x, str) else x)
+        df.set_index('sample', inplace=True)
+        if condition2 and sample_condition2:
+            df['condition2'] = sample_condition2
+        return df
+    else:
+        raise ValueError("Could not find the specified accessions or condition in the file.")
+        return None
