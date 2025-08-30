e620d41bc488ea050051969bb05d7fa3ed4f3002
Ryland <rybednarek@gmail.com>
2025-08-29 21:07:41 -0700

    deseq2 function added

--- analysis_modules/de_analysis.py
+++ analysis_modules/de_analysis.py
@@ -2,6 +2,12 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import pandas as pd
+import rpy2.robjects as ro
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri, default_converter
+from rpy2.robjects.conversion import localconverter
+
 
 def plot_count_pca(count_df, 
                    raw_count=10, 
@@ -13,6 +19,22 @@ def plot_count_pca(count_df,
                    col1=None,
                    col2=None,
                    exp=None):
+    '''Perform PCA on count data and plot the results.
+    Inputs:
+    - count_df: Raw count DataFrame
+    - raw_count: Gene must have minimum count of raw_count in at least (min_samples) to be considered when calculating PCA
+    - min_samples: Minimum number of samples a gene must be expressed in at (raw_count levels) to be considered
+    - log: Whether to log-transform the data
+    - plot: Whether to plot the PCA results
+    - meta_df: Optional metadata DataFrame for coloring points in the PCA plot (optional)
+    - col1: Name of the first column in meta_df to use for coloring points - (if meta_df provided, not optional)
+    - col2: Name of the second column in meta_df to use for shaping points (optional)
+    - exp: name of the experiment (optional)
+    Output:
+    - pca_df: DataFrame with PCA results
+    - pc1_variance: Variance explained by PC1
+    - pc2_variance: Variance explained by PC2
+    '''
     pca = PCA(n_components=components)
     count_filter = count_df.loc[:, (count_df > raw_count).sum(axis=0) >= min_samples]
     if log:
@@ -48,3 +70,32 @@ def plot_count_pca(count_df,
         plt.show()
     return pca_df, pc1_variance, pc2_variance
 
+
+def run_de_analysis(counts_df, meta_df, group1, group2, cat_col='group'):
+    '''Run differential expression analysis.
+    Inputs:
+    - counts_df: Raw count DataFrame with column names matching meta_df row names
+    - meta_df: Sample metadata with column defining sample groups (default 'group') and row names matching counts_df column names
+    - group1: Name of the first group for comparison
+    - group2: Name of the second group for comparison
+    - cat_col: Name of the column in meta_df that defines the sample groups (default 'group')
+    Outputs:
+    - res_df: DataFrame with differential expression results
+    '''
+    base = importr('base')
+    deseq2 = importr('DESeq2')
+    meta_df[cat_col] = pd.Categorical(meta_df[cat_col], categories=[group1, group2])
+    with localconverter(default_converter + pandas2ri.converter):
+        r_counts_df = pandas2ri.py2rpy(counts_df)
+        r_meta_df = pandas2ri.py2rpy(meta_df)
+    dds = deseq2.DESeqDataSetFromMatrix(
+        countData=r_counts_df,
+        colData=r_meta_df,
+        design=ro.Formula('~ group')
+    )
+    dds = deseq2.DESeq(dds)
+    res = deseq2.results(dds)
+    res_df_r = base.as_data_frame(res)
+    with localconverter(default_converter + pandas2ri.converter):
+        res_df = pandas2ri.rpy2py(res_df_r)
+    return res_df
